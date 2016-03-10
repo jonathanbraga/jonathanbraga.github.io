cbc1930cc3e75c83359a6c2d6886f93869343576
Jonathan Braga <jonathanb2br@gmail.com>
2016-03-10 00:12:00 -0300

    atualização do codigo

---
+++ js/direction.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+			$('#fullpage').fullpage({
+				anchors: ['home', 'about', '3rdPage','aaa'],
+				sectionsColor: ['#000', '#1BBC9B', '#7E8F7C'],
+				responsiveHeight: 600
+			});
+		});
