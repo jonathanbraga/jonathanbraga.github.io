559aaba9e094d4d79b53abe1276abe4bf6482bf6
Jonathan Braga <jonathanb2br@gmail.com>
2016-03-11 01:19:55 -0300

    scroll bug solve

--- js/direction.js
+++ js/direction.js
@@ -3,6 +3,6 @@
 			$('#fullpage').fullpage({
 				anchors: ['home', 'about', 'skills','aaa'],
 				sectionsColor: ['#000', '#1589A9', '#1589A9','#1589A9'],
-				responsiveHeight: 600
+				//responsiveHeight: 600
 			});
 		});
