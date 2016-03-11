055e324cf82de16ae0bbd09a53433f9c51225c65
Jonathan Braga <jonathanb2br@gmail.com>
2016-03-11 01:16:05 -0300

    skills atualizado

--- js/direction.js
+++ js/direction.js
@@ -1,7 +1,8 @@
-$(document).ready(function() {
+
+		$(document).ready(function() {
 			$('#fullpage').fullpage({
-				anchors: ['home', 'about', '3rdPage','aaa'],
-				sectionsColor: ['#000', '#1BBC9B', '#7E8F7C'],
+				anchors: ['home', 'about', 'skills','aaa'],
+				sectionsColor: ['#000', '#1589A9', '#1589A9','#1589A9'],
 				responsiveHeight: 600
 			});
 		});
