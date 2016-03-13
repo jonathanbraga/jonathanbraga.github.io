a084dccd20b21298e7407f379939ae0e4766b616
Jonathan Braga <jonathanb2br@gmail.com>
2016-03-13 15:31:56 -0300

    bug solve: mobile icon skills

--- js/direction.js
+++ js/direction.js
@@ -1,7 +1,7 @@
 
 		$(document).ready(function() {
 			$('#fullpage').fullpage({
-				anchors: ['home', 'about', 'skills','aaa'],
+				anchors: ['home', 'about', 'skills','contact'],
 				sectionsColor: ['#000', '#1589A9', '#1589A9','#1589A9'],
 				scrollOverflow: true
 			});
