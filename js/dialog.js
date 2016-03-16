8f5b02b470d041c19fe511abd35177b37025959f
Jonathan Braga <jonathanb2br@gmail.com>
2016-03-16 10:49:30 -0300

    modal inculde

---
+++ js/dialog.js
@@ -0,0 +1,25 @@
+		$(document).ready(function() {
+			
+					$(function() {
+    $( "#dialog" ).dialog({
+      autoOpen: false,
+      show: {
+        effect: "blind",
+        duration: 1000
+      },
+      hide: {
+        effect: "explode",
+        duration: 1000
+      }
+    });
+ 
+    $( "#opener" ).click(function() {
+      $( "#dialog" ).dialog( "open" );
+    });
+  });
+			
+					
+		});
+		
+		
+
--- js/direction.js
+++ js/direction.js
@@ -4,5 +4,5 @@
 				anchors: ['home', 'about', 'skills','contact'],
 				sectionsColor: ['#000', '#1589A9', '#1589A9','#1589A9'],
 				scrollOverflow: true
-			});
+			}); 
 		});
