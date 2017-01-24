7548f62cc42589535cc1a918359969a566f4f733
Jonathan Braga <Jonathan Braga>
2017-01-24 13:23:50 -0200

    show source code

--- antigo/js/direction.js
+++
@@ -1,8 +0,0 @@
-
-		$(document).ready(function() {
-			$('#fullpage').fullpage({
-				anchors: ['home', 'about', 'skills','contact'],
-				sectionsColor: ['#000', '#1589A9', '#1589A9','#1589A9'],
-				scrollOverflow: true
-			}); 
-		});
